2b00c0d8d7df3ee63bcfcc9f27ba1b406bcf547c
tomoko3seven <sqvizers@gmail.com>
2026-03-02 00:05:24 +0100

    abydos first time teleporter

--- kubejs/server_scripts/abydos_teleporter.js
+++ kubejs/server_scripts/abydos_teleporter.js
@@ -5,16 +5,21 @@ PlayerEvents.loggedIn(event => {
         player.persistentData.first_join_abydos = true;
 
         event.server.scheduleInTicks(60, () => {
-            
-            player.health = 6;
+    
+    player.health = 6;
+    event.server.runCommandSilent(`execute in sgjourney:abydos run tp "${player.username}" 0 100 0`);
+    
+    event.server.runCommandSilent(`execute in ${dim} run setworldspawn ${spawnX} ${spawnY} ${spawnZ}`);
 
-            //forge dimensions
-            event.server.runCommandSilent(`execute in sgjourney:abydos run tp "${player.username}" 0 100 0`);
-            
-            event.server.runCommandSilent(`effect give "${player.username}" minecraft:resistance 15 4 true`);
-            event.server.runCommandSilent(`effect give "${player.username}" minecraft:slow_falling 15 0 true`);
-            
-            player.tell("§bCondition: Critical! Get to the medical center immediately!");
-        });
+    let suit = Item.of('ad_astra:netherite_space_suit', '{BotariumData:{StoredFluids:[{Amount:10000L,Fluid:"ad_astra:oxygen"}]}}');
+    
+    player.setChestArmorItem(suit);
+    player.setHeadArmorItem('ad_astra:netherite_space_helmet');
+    player.setLegsArmorItem('ad_astra:netherite_space_pants');
+    player.setFeetArmorItem('ad_astra:netherite_space_boots');
+
+    player.tell("§bCondition: Critical! Get to the medical center immediately!");
+    player.tell("§eLife support system activated. Oxygen supply: 100%.");
+});
     }
 });
