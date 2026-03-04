0746d9e3ad3ab7031977f197fae72f1fe2ca4f50
tomoko3seven <sqvizers@gmail.com>
2026-03-04 19:10:45 +0100

    now creates ship

--- kubejs/server_scripts/abydos_teleporter.js
+++ kubejs/server_scripts/abydos_teleporter.js
@@ -1,35 +1,37 @@
 PlayerEvents.loggedIn(event => {
-    const player = event.player;
+    const { player, server } = event;
 
     if (!player.persistentData.first_join_abydos) {
         player.persistentData.first_join_abydos = true;
 
-        event.server.scheduleInTicks(60, () => {
-    
-    player.health = 6;
+        server.scheduleInTicks(60, () => {
+            const dim = 'sgjourney:abydos';
+            const shipX = -22; 
+            const shipY = 75;
+            const shipZ = -22;
 
-    const shipX = -5; 
-    const shipY = 98;
-    const shipZ = -5;
+            server.runCommandSilent(`execute in ${dim} run tp "${player.username}" 16 83 10`);
+            server.runCommandSilent(`execute in ${dim} run setworldspawn 16 83 10`);
+            
+            player.health = 6;
+            let suit = Item.of('ad_astra:netherite_space_suit', '{BotariumData:{StoredFluids:[{Amount:10000L,Fluid:"ad_astra:oxygen"}]}}');
+            player.setChestArmorItem(suit);
+            player.setHeadArmorItem('ad_astra:netherite_space_helmet');
+            player.setLegsArmorItem('ad_astra:netherite_space_pants');
+            player.setFeetArmorItem('ad_astra:netherite_space_boots');
 
-    event.server.runCommandSilent(`forceload add ${shipX} ${shipZ}`);
+            player.tell("§bCondition: Critical! Get to the medical center immediately!");
 
-    event.server.runCommandSilent(`execute in sgjourney:abydos run tp "${player.username}" 0 100 0`);
-    event.server.runCommandSilent(`execute in sgjourney:abydos run place template abydos:broken_spaceship ${shipX} ${shipY} ${shipZ} none none`);
-
-    
-    event.server.runCommandSilent(`execute in sgjourney:abydos run setworldspawn 0 100 0`);
-    event.server.runCommandSilent(`forceload remove ${shipX} ${shipZ}`);
-
-    let suit = Item.of('ad_astra:netherite_space_suit', '{BotariumData:{StoredFluids:[{Amount:10000L,Fluid:"ad_astra:oxygen"}]}}');
-    
-    player.setChestArmorItem(suit);
-    player.setHeadArmorItem('ad_astra:netherite_space_helmet');
-    player.setLegsArmorItem('ad_astra:netherite_space_pants');
-    player.setFeetArmorItem('ad_astra:netherite_space_boots');
-
-    player.tell("§bCondition: Critical! Get to the medical center immediately!");
-    player.tell("§eLife support system activated. Oxygen supply: 100%.");
-});
+            server.scheduleInTicks(20, () => {
+                server.runCommandSilent(`execute in ${dim} run forceload add ${shipX} ${shipZ}`);
+                
+                server.runCommandSilent(`execute in ${dim} run place template abydos:broken_ship ${shipX} ${shipY} ${shipZ} none none`);
+                
+                server.runCommandSilent(`execute in ${dim} run forceload remove ${shipX} ${shipZ}`);
+                player.tell("§eLife support system activated. Oxygen supply: 100%.");
+                
+                server.runCommandSilent(`execute in ${dim} at "${player.username}" run playsound minecraft:entity.iron_golem.damage ambient @a ~ ~ ~ 1 0.5`);
+            });
+        });
     }
 });
