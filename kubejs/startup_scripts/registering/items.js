33f47311a04650cd23c9c0fa6159c30718cfc564
tomoko3seven <sqvizers@gmail.com>
2026-03-04 23:32:17 +0100

    bullershit

--- kubejs/startup_scripts/registering/items.js
+++
@@ -1,9 +0,0 @@
-StartupEvents.registry('item', event => {
-    // Register new items here
-    event.create('torchflower_stem');
-    event.create('plant_fiber');
-    event.create('plant_fiber_mesh');
-    event.create('primitive_sieve');
-
-    event.create('hot_glass');
-});
